a7c4eb4d81badb289464b46f6472d892bdf2678b
1pccx <121043242+1pccx@users.noreply.github.com>
2025-04-29 21:30:31 +0800

    Create InsertionSort.cpp

---
+++ homework/code/InsertionSort.cpp
@@ -0,0 +1,12 @@
+// 插入排序
+void insertionSort(vector<int>& arr) {
+    for (int i = 1; i < (int)arr.size(); i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}

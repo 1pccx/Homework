67034b459df2d6dc1d48fe401cd0201ee76b42e8
1pccx <121043242+1pccx@users.noreply.github.com>
2025-04-29 21:33:55 +0800

    Create MergeSort.cpp

---
+++ homework/code/MergeSort.cpp
@@ -0,0 +1,29 @@
+// 合併排序
+void merge(vector<int>& arr, int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int i = 0; i < n2; i++) R[i] = arr[m + 1 + i];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSortIterative(vector<int>& arr) {
+    int n = arr.size();
+    for (int size = 1; size < n; size = 2 * size) {
+        for (int left = 0; left < n - 1; left += 2 * size) {
+            int mid = min(n - 1, left + size - 1);
+            int right = min((left + 2 * size - 1), (n - 1));
+            if (mid < right) {
+                merge(arr, left, mid, right);
+            }
+        }
+    }
+}

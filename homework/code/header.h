04a3be0c6e3504bd7d2fd6f435163103adf9030e
1pccx <121043242+1pccx@users.noreply.github.com>
2025-04-29 21:29:03 +0800

    Update header.h

--- homework/code/header.h
+++ homework/code/header.h
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib> // 包含 srand() 和 rand()
+#include <ctime>   // 包含 time()
+#include <chrono>  // 包含 chrono 用於計時
+#include <fstream> // 用於檔案操作
 #include <algorithm>
-#include <ctime>
-#include <cstdlib>
-#include <fstream>
 #include <map>
-#include <random>
-#include <chrono>
-#include <Windows.h>
-#include <Psapi.h>
+
+using namespace std;
+using namespace chrono;
